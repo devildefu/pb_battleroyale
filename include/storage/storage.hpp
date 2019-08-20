71515517c76bfd600d1c340bd315fab44108fef6
beProsto <48763903+beProsto@users.noreply.github.com>
2019-08-20 20:06:00 +0200

    Corrected my fork

--- include/storage/storage.hpp
+++ include/storage/storage.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <SFML/Graphics.hpp>
 
 namespace res {
 
@@ -15,6 +16,38 @@ namespace res {
 		Storage() {}
 		Storage(const Storage& other)
 		 : items(other.items) {}
+
+		~Storage() {}
+
+
+		//enter the arguments to the constructor after the key (if none, leave only the key)
+		template<typename ... Args>
+		inline void add(const std::string& key, const Args&...);
+
+		inline void remove(const std::string& key);
+
+		inline void clear();
+
+		inline int size();
+
+		// returns the last item if the key does not match to anything
+		T get(const std::string& n);
+
+		// will create a new item if the key does not match to anything
+		T operator[](const std::string& n);
+
+	};
+	
+	class TextureManager {
+	private:
+
+		std::map<std::string, sf::Texture*> items;
+		
+	public:
+
+		Storage() {}
+		Storage(const Storage& other)
+		 : items(other.items), names(other.names) {}
 
 		~Storage() {}
 
