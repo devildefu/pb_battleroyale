b6147c7b0d08b92c8c72380b0a61dcfdc59388d6
Devourer of Source <panczerwonyser@gmail.com>
2019-08-20 12:47:55 +0200

    Add comments

--- include/eventhandler.hpp
+++ include/eventhandler.hpp
@@ -4,11 +4,28 @@
 
 class EventHandler {
 public:
+	/**
+	 * Called upon each logic update to the game.
+	 */
 	void update() { };
+
+	/**
+	 * Called to do the drawing of your game.
+	 */
 	void draw() { };
-	
+
+	/**
+	 * Called when current handler will be changed to another
+	 */
 	void clear() { };
+
+	/**
+	 * Called when current handler is changed
+	 */
 	void init() { };
 
+	/**
+	 * Called when window is closed
+	 */
 	void event_quit(sf::RenderWindow* window) { window->close(); }
 };
