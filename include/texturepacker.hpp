d2c26f36bf644e779840f09724db092165e1ba91
iLiquid <liquidekgaming@gmail.com>
2019-09-11 19:30:11 +0200

    Texture packer (#7)

---
+++ include/texturepacker.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <vector>
+
+class TexturePacker {
+public:
+	TexturePacker(sf::Texture& target_image);
+
+	sf::IntRect pack(sf::Texture& subimage);
+private:
+	sf::Texture& target;
+	std::vector<sf::IntRect> occupied;
+	int x, y;
+
+	bool is_free(sf::IntRect& area);
+};
