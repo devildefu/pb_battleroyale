38b6edee48360840c976b584da1aed8ed3667b72
liquid600pgm <liquidekgaming@gmail.com>
2019-09-11 17:58:47 +0200

    Swapped public: and private: sections

--- include/texturepacker.hpp
+++ include/texturepacker.hpp
@@ -5,14 +5,14 @@
 #include <vector>
 
 class TexturePacker {
+public:
+	TexturePacker(sf::Texture& target_image);
+
+	sf::IntRect pack(sf::Texture& subimage);
 private:
 	sf::Texture& target;
 	std::vector<sf::IntRect> occupied;
 	int x, y;
 
 	bool is_free(sf::IntRect& area);
-public:
-	TexturePacker(sf::Texture& target_image);
-
-	sf::IntRect pack(sf::Texture& subimage);
 };
