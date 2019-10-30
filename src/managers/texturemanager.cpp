2235a34c8e199378ca8857e416b5228efb6c2de6
Firstbober <firstbober@mail.ru>
2019-10-30 16:42:21 +0100

    Added load, remove, clear, exists functions to TM.

--- src/managers/texturemanager.cpp
+++ src/managers/texturemanager.cpp
@@ -17,10 +17,52 @@ sf::Texture& impl::TextureManager::get(std::string texture) {
 			SPDLOG_ERROR("Cannot load {} texture file", texture + ".png");
 			txt->loadFromMemory(invalidtexture, size_invalidtexture);
 		}
-		
+
 		this->textures[texture] = txt;
 		return *txt;
 	} else {
 		return *this->textures[texture];
+	}
+}
+
+bool impl::TextureManager::load(std::string texture) {
+	SPDLOG_INFO("Loading texture \"{}\"", texture);
+	if(this->textures.find(texture) == this->textures.end()) {
+		sf::Texture* txt = new sf::Texture();
+
+		if(!txt->loadFromFile(texture + ".png")) {
+			SPDLOG_ERROR("Cannot load {} texture file", texture + ".png");
+			txt->loadFromMemory(invalidtexture, size_invalidtexture);
+			return false;
+		}
+
+		this->textures[texture] = txt;
+		return true;
+	} else {
+		return true;
+	}
+}
+
+bool impl::TextureManager::remove(std::string texture) {
+	if(this->textures.find(texture) == this->textures.end()) {
+		return false;
+	} else {
+		delete this->textures[texture];
+		this->textures.erase(texture);
+		return true;
+	}
+}
+
+void impl::TextureManager::clear() {
+	for(auto&& [key, val] : this->textures) {
+		delete val;
+	}
+}
+
+bool impl::TextureManager::exists(std::string texture) {
+	if(this->textures.find(texture) == this->textures.end()) {
+		return false;
+	} else {
+		return true;
 	}
 }
