21b16a447d4e03540fe9bb9be7d6e45c4269e77c
Devourer of Source <panczerwonyser@gmail.com>
2019-12-04 15:13:55 +0100

    Fix build using MSVC

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,6 +1,11 @@
 #include <helpers.hpp>
 
 bool Helpers::file_exists(const char* fname) {
-	struct stat buffer;
-	return (stat(fname, &buffer) == 0);
+	#ifdef _MSC_VER
+		DWORD attr = GetFileAttributesA(fname);
+		return (attr != INVALID_FILE_ATTRIBUTES && !(attr & FILE_ATTRIBUTE_DIRECTORY));
+	#else
+		struct stat buffer;
+		return (stat(fname, &buffer) == 0);
+	#endif
 }
--- src/helpers.hpp
+++ src/helpers.hpp
@@ -1,7 +1,11 @@
 #pragma once
-
-#include <sys/stat.h>
+#ifdef _MSC_VER
+// MSVC doesn't have a unistd.h header
+#include <windows.h>
+#else
 #include <unistd.h>
+#include <sys/stat.h>
+#endif
 
 namespace Helpers {
 	bool file_exists(const char* fname);
